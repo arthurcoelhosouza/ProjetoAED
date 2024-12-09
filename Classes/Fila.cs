e01d775f8bbe583f08e4ced057ad81f07df7b107
ramaciel <ricardoamaciel2016@gmail.com>
2024-12-09 18:27:07 -0300

    mudancas classe fila.cs

--- Classes/Fila.cs
+++ Classes/Fila.cs
@@ -8,47 +8,28 @@ using System.Threading.Tasks;
 namespace ProjetoAED.Classes {
     internal class Fila {
         private Candidato[] fila;
-        private int inicio, fim, tamanhoAtual, capacidade;
+        private int inicio, fim;
 
         public Fila(int capacidade) {
-            this.capacidade = capacidade;
-            fila = new Candidato[capacidade];
-            inicio = 0;
-            fim = -1;
-            tamanhoAtual = 0;
+            fila = new Candidato[capacidade + 1];
+            inicio = fim = 0;
         }
-
         public void Inserir(Candidato candidato)
         {
-            if (EstaCheia()) { return; }
-
-            fim = (fim + 1) % capacidade;
+            if (((fim + 1) % fila.Length) == primeiro)
+            {
+                return;
+            }
             fila[fim] = candidato;
-            tamanhoAtual++;
+            fim = (fim + 1) % fila.Length;
         }
-
         public void Remover()
         {
-            if (EstaVazia()) { return; }
-
-            Candidato candidato = fila[inicio];
-            inicio = (inicio + 1) % capacidade;
-            tamanhoAtual--;
-        }
-
-        public bool EstaCheia()
-        {
-            if (tamanhoAtual == capacidade) { return true; }
-        }
-
-        public bool EstaVazia()
-        {
-            if (tamanhoAtual == 0) { return true; }
-        }
-
-        public int TamanhoAtual()
-        {
-            return tamanhoAtual;
+            if(inicio == fim)
+            {
+                return;
+            }
+            inicio = (inicio + 1) % fila.Length;
         }
     }
 }
