b9b75d88344920022f006ae0520c8e773e4791c3
Melissa Duque Baía <116390230+melduque@users.noreply.github.com>
2024-12-09 18:09:46 -0300

    Update Curso.cs

--- Classes/Curso.cs
+++ Classes/Curso.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +9,20 @@ namespace ProjetoAED.Classes{
     internal class Curso{
         private int codCurso, qtdVagas;
         private string nome;
+        private List<Candidato> listaSelecionados;
+        private Fila filadeEspera;
+        private double notaDeCorte;
+        private int tamanhoEspera = 10;
+
         public Curso(int codCurso, string nome, int qtdVagas){
             this.codCurso = codCurso;
             this.qtdVagas = qtdVagas;
             this.nome = nome;
+            this.listaSelecionados = new List<Candidato>();
+            this.filadeEspera = new Fila(tamanhoEspera);
+            this.notaDeCorte = 0.0;
         }
+
         public int CodCurso{ 
             get { return codCurso; }
             set { codCurso = value; }
@@ -24,6 +34,70 @@ namespace ProjetoAED.Classes{
         public string Nome{
             get { return nome; }
             set { nome = value; }
+        }
+        public double NotaDeCorte
+        {
+            get { return notaDeCorte; }
+            set { notaDeCorte = value; }
+        }
+
+        public List<Candidato> ListaSelecionados
+        {
+            get { return listaSelecionados; }
+        }
+
+        public Fila FilaDeEspera
+        {
+            get { return filadeEspera; }
+        }
+
+        public bool EstaCheio()
+        {
+            return listaSelecionados.Count >= qtdVagas;
+        }
+
+        public void InserirCandidato(Candidato candidato)
+        {
+            if (!EstaCheio())
+            {
+                listaSelecionados.Add(candidato);
+                AtualizarNotaDeCorte();
+            }
+            else
+            {
+                filadeEspera.Inserir(candidato);
+            }
+        }
+
+        public void AtualizarNotaDeCorte()
+        {
+            if (listaSelecionados.Count > 0)
+            {
+                double menorNota = listaSelecionados[0].NotaMedia;
+
+                foreach (var candidato in listaSelecionados)
+                {
+                    if (candidato.NotaMedia < menorNota)
+                    {
+                        menorNota = candidato.NotaMedia;
+                    }
+                }
+                notaDeCorte = menorNota;
+            }
+            else
+            {
+                notaDeCorte = 0.0;
+            }
+        }
+
+        public void ProcessarFilaDeEspera()
+        {
+            while (!filadeEspera.EstaVazia() && !EstaCheio())
+            {
+                Candidato candidato = filadeEspera.Remover();
+                listaSelecionados.Add(candidato);
+                AtualizarNotaDeCorte();
+            }
         }
     }
 }
