37e0434e49dda134d1860e819a7a31e51c8ed14a
ramaciel <ricardoamaciel2016@gmail.com>
2024-12-10 12:32:48 -0300

    add atributo aprovado

--- Classes/Candidato.cs
+++ Classes/Candidato.cs
@@ -5,13 +5,17 @@ using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjetoAED.Classes {
-    internal class Candidato {
+namespace ProjetoAED.Classes
+{
+    internal class Candidato
+    {
         private string nome;
         private double notaMedia, notaRedacao, notaMatematica, notaLinguagens;
         private int codCurso1, codCurso2;
-    
-        public Candidato(string nome, double notaRedacao, double notaMatematica, double notaLinguagens, int codCurso1, int codCurso2){ 
+        private bool aprovado1, aprovado2;
+
+        public Candidato(string nome, double notaRedacao, double notaMatematica, double notaLinguagens, int codCurso1, int codCurso2)
+        {
             this.nome = nome;
             this.notaRedacao = notaRedacao;
             this.notaMatematica = notaMatematica;
@@ -20,23 +24,28 @@ namespace ProjetoAED.Classes {
             this.codCurso2 = codCurso2;
             this.notaMedia = (notaRedacao + notaMatematica + notaLinguagens) / 3;
         }
-        public string Nome{ 
-            get { return nome; } 
+        public string Nome
+        {
+            get { return nome; }
             set { nome = value; }
         }
-        public double NotaMedia{
-            get { return notaMedia; } 
-            set {   notaMedia = value; } 
+        public double NotaMedia
+        {
+            get { return notaMedia; }
+            set { notaMedia = value; }
         }
-        public double NotaRedacao{ 
-            get { return notaRedacao; } 
-            set { notaRedacao = value; } 
+        public double NotaRedacao
+        {
+            get { return notaRedacao; }
+            set { notaRedacao = value; }
         }
-        public double NotaMatematica{
+        public double NotaMatematica
+        {
             get { return notaMatematica; }
             set { notaMatematica = value; }
         }
-        public double NotaLinguagens{
+        public double NotaLinguagens
+        {
             get { return notaLinguagens; }
             set { notaLinguagens = value; }
         }
@@ -49,6 +58,16 @@ namespace ProjetoAED.Classes {
         {
             get { return CodCurso2; }
             set { CodCurso2 = value; }
+        }
+        public bool Aprovado1
+        {
+            get { return aprovado1; }
+            set { aprovado1 = value; }
+        }
+        public bool Aprovado2
+        {
+            get { return aprovado2; };
+            set { aprovado2 = value; };
         }
     }
 }
